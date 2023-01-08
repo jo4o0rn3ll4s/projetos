719efc3126cdbd900f8f1cf51f301854bec5d294
João Ornellas <65920201+jo4o0rn3ll4s@users.noreply.github.com>
2023-01-08 01:04:17 -0300

    Update valida_cpf_func.py

--- valida_cpf_func.py
+++ valida_cpf_func.py
@@ -1,29 +1,42 @@
 def valicpf(cpf):
+    while cpf <= 11111111111 or cpf >= 99999999999:
+        if cpf <= 11111111111 or cpf >= 99999999999:
+            i = int(input('CPF: '))
+            cpf = i
+    return i
+
+def vericpf(cpf):
     sep = list()
     num = list()
     ckcpf = list()
     val = [0, 0, 0]
     dig = [0, 0, 0]
-
+     
+    #separa o numero com 11 casas decimais em uma lista com os digitos independentes
     for i in range(0,11):
         sep.insert(i, cpf//10**(10-i))
-
+     
     num.insert(0,sep[0])
     for i in range(1,11):
         num.insert(i, sep[i]-sep[i-1]*10)
-
+        
+    #começa a etapa de verificação dos digitos
+     
+    #laço para reaçização dos testes nos 2 digitos finais
     for i in range(2, 0, -1):
         som = 0
         multi = 10 if i == 2 else 11
+        #faz a multiplicação em decaimento partindo de 10 e depois de 11 a 2
         for j in range(0, 11-i, 1):
             ckcpf.insert(j, num[j]*multi)
             som = som + ckcpf[j]
             multi = multi - 1
+        #armazenamento do valor calculado
         val.insert(i, som % 11)
         dig.insert(i, 0 if val[i] >= 10 else 11 - val[i])
         ckcpf.clear()
+    #resposta da função
     return True if num[9] == dig[3] and num[10] == dig[1] else False
-
 
 def debugcpf(cpf):
     sep = list()
@@ -31,14 +44,14 @@ def debugcpf(cpf):
     ckcpf = list()
     val = [0,0]
     dig = [0,0]
-
+     
     for i in range(0,11):
         sep.insert(i, cpf//10**(10-i))
-
+     
     num.insert(0,sep[0])
     for i in range(1,11):
         num.insert(i, sep[i]-sep[i-1]*10)
-
+     
     for i in range(2, 0, -1):
         som = 0
         multi = 10 if i == 2 else 11
@@ -52,3 +65,4 @@ def debugcpf(cpf):
         print('Digito\n som {}\n val {}\n dig {}\n'.format(som, val, dig))
     print('Digitado {} {} - Calculado {} {}'.format(num[9],num[10],dig[3],dig[1]))
     print('CPF valido' if num[9] == dig[3] and num[10] == dig[1] else 'CPF invalido')
+
